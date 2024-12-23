a68e9b6558bcf2faf0818f669b3491141a5f80fa
Aditya Kumar <git@adityais.dev>
2024-12-23 14:15:36 +0000

    Added more arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,31 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 pub mod rust_doc;
 
 #[derive(Parser, Debug)]
+#[command(name = "cargo wiki")]
 pub struct Configuration {
+    #[arg(short, long, default_value_t = String::from(""))]
+    pub package: String,
+    #[arg(long, default_value_t = false)]
+    pub workspace: bool,
     #[arg(long, default_value_t = false)]
     pub document_private_items: bool,
     #[arg(long, default_value_t = false)]
     pub no_deps: bool,
+    #[arg(short, long, value_enum, default_value_t = MarkdownFlavor::GitHub)]
+    pub markdown_flavor: MarkdownFlavor,
+    // TODO: Add more arguments to support more feautures
+    // refer: https://doc.rust-lang.org/cargo/commands/cargo-doc.html
+    // refer: https://doc.rust-lang.org/cargo/commands/cargo-rustdoc.html
+}
+
+/// Markdown Flavor to be used
+#[derive(Debug, ValueEnum, Clone)]
+pub enum MarkdownFlavor {
+    /// GitHub Markdown Flavor
+    GitHub,
+    /// GitLab Markdown Flavor
+    GitLab,
+    // TODO: Add support for other markdown flavors
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use clap::Parser;
+use clap::{CommandFactory, Parser};
 use cargo_wiki::Configuration;
 use cargo_wiki::rust_doc::generate_rust_doc;
 
 fn main() {
-    let args = std::env::args().skip(2).collect::<Vec<String>>();
-    let configuration = Configuration::parse_from(args);
+    let args = std::env::args().collect::<Vec<String>>();
+
+    if args.len() > 1 && (args[1] == "--help" || args[1] == "-h") {
+        Configuration::command().print_help().unwrap();
+        return;
+    }
+
+    let configuration = Configuration::parse_from(&args[1..]);
 
     println!("{:#?}", configuration);
     generate_rust_doc(&configuration).expect("Failed to generate rust doc");
