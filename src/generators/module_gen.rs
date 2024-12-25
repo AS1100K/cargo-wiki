5d6ba9b3fdc153b43b623f191bc23101c2e1bc15
Aditya Kumar <git@adityais.dev>
2024-12-25 13:04:00 +0000

    Added Documentation for modules

--- src/generators/module_gen.rs
+++ src/generators/module_gen.rs
@@ -5,7 +5,45 @@ use anyhow::Result;
 use rustdoc_types::{Item, ItemEnum, Module};
 use std::fs;
 
-pub const MODULE_FILE_NAME: &str = "README.md";
+pub const MODULE_FILE_NAME: &str = "README";
+
+#[derive(Default)]
+pub struct ModuleItems<'a> {
+    pub modules: Vec<ModuleField<'a>>,
+    pub traits: Vec<ModuleField<'a>>,
+    pub functions: Vec<ModuleField<'a>>,
+    pub macros: Vec<ModuleField<'a>>,
+    pub re_exports: Vec<ModuleField<'a>>,
+    pub structs: Vec<ModuleField<'a>>,
+    pub enums: Vec<ModuleField<'a>>,
+    pub consts: Vec<ModuleField<'a>>,
+}
+
+pub struct ModuleField<'a> {
+    pub name: &'a str,
+    pub link: String,
+    pub description: &'a str,
+}
+
+impl<'a> ModuleField<'a> {
+    pub fn to_string(&self) -> String {
+        let mut field_string = String::new();
+
+        field_string.push_str("- [`");
+        field_string.push_str(self.name);
+        field_string.push_str("`](");
+        field_string.push_str(&self.link);
+        field_string.push_str(")\n");
+
+        if !self.description.is_empty() {
+            field_string.push_str("\n\t");
+            field_string.push_str(self.description);
+            field_string.push_str("\n");
+        }
+
+        field_string
+    }
+}
 
 pub struct ModuleGenerator<'a> {
     pub prefix_path: String,
@@ -54,6 +92,7 @@ impl<'a> ModuleGenerator<'a> {
         };
         let mut path = format!("{}/{}", self.prefix_path, module_name);
         let mut module_file_content = format!("# {}\n\n", module_name);
+        let mut module_information = ModuleItems::default();
 
         gen_path(&path)?;
 
@@ -68,11 +107,22 @@ impl<'a> ModuleGenerator<'a> {
                 continue;
             };
 
+            let item_description = match &item.docs {
+                Some(doc) => &doc[..doc.len().min(50)],
+                None => ""
+            };
+
             let mut path = format!("{}", path);
             let mut file_content = format!("# {}\n\n", item_name);
 
             match &item.inner {
                 ItemEnum::Module(_) => {
+                    module_information.modules.push(ModuleField {
+                        name: item_name,
+                        link: format!("./{}/{}.md", item_name, self.module_file_name),
+                        description: &item_description,
+                    });
+
                     let new_module_generator = Self::new(
                         path,
                         &self.module_file_name,
@@ -86,6 +136,12 @@ impl<'a> ModuleGenerator<'a> {
                     continue;
                 }
                 ItemEnum::Struct(_) => {
+                    module_information.structs.push(ModuleField {
+                        name: item_name,
+                        link: format!("./struct.{}.md", item_name),
+                        description: &item_description,
+                    });
+
                     path.push_str("/struct.");
                     path.push_str(item_name);
 
@@ -105,6 +161,85 @@ impl<'a> ModuleGenerator<'a> {
                 .expect("TODO: panic message at `src/generators/module_gen.rs`");
         }
 
+        path.push_str("/");
+        path.push_str(self.module_file_name);
+        path.push_str(".md");
+
+        module_file_content.push_str(&Self::generate_module_docs(&module_information));
+        fs::write(path, module_file_content).expect(
+            "TODO: panic message at `src/generators/module_gen.rs` while saving module file",
+        );
+
         Ok(())
+    }
+
+    pub fn generate_module_docs(module_information: &ModuleItems) -> String {
+        let mut module_information_string = String::new();
+
+        // Modules
+        if module_information.modules.len() > 0 {
+            module_information_string.push_str("\n## Modules\n\n");
+            for field in &module_information.modules {
+                module_information_string.push_str(&field.to_string());
+            }
+        }
+
+        // Constants
+        if module_information.consts.len() > 0 {
+            module_information_string.push_str("## Constants\n\n");
+            for field in &module_information.consts {
+                module_information_string.push_str(&field.to_string());
+            }
+        }
+
+        // Structs
+        if module_information.structs.len() > 0 {
+            module_information_string.push_str("## Structs\n\n");
+            for field in &module_information.structs {
+                module_information_string.push_str(&field.to_string());
+            }
+        }
+
+        // Enums
+        if module_information.enums.len() > 0 {
+            module_information_string.push_str("## Enums\n\n");
+            for field in &module_information.enums {
+                module_information_string.push_str(&field.to_string());
+            }
+        }
+
+        // Traits
+        if module_information.traits.len() > 0 {
+            module_information_string.push_str("## Traits\n\n");
+            for field in &module_information.traits {
+                module_information_string.push_str(&field.to_string());
+            }
+        }
+
+        // Functions
+        if module_information.functions.len() > 0 {
+            module_information_string.push_str("## Functions\n\n");
+            for field in &module_information.functions {
+                module_information_string.push_str(&field.to_string());
+            }
+        }
+
+        // Macros
+        if module_information.macros.len() > 0 {
+            module_information_string.push_str("## Macros\n\n");
+            for field in &module_information.macros {
+                module_information_string.push_str(&field.to_string());
+            }
+        }
+
+        // Re-exports
+        if module_information.re_exports.len() > 0 {
+            module_information_string.push_str("## Re-exports\n\n");
+            for field in &module_information.re_exports {
+                module_information_string.push_str(&field.to_string());
+            }
+        }
+
+        module_information_string
     }
 }
