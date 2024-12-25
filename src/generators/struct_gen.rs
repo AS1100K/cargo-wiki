3cb59139ad3f36d56d80a6ac6995927b17b5f9a9
Aditya Kumar <git@adityais.dev>
2024-12-25 09:16:52 +0000

    Completed Syntax Generation for every `StructKind`

--- src/generators/struct_gen.rs
+++ src/generators/struct_gen.rs
@@ -1,4 +1,5 @@
 use crate::generators::generic_gen::GenericGenerator;
+use crate::generators::type_gen::TypeGenerator;
 use crate::generators::visibility_gen::VisibilityGenerator;
 use crate::generators::{ExternalCrates, Generator, Index, Paths};
 use anyhow::Result;
@@ -26,28 +27,81 @@ impl Generator for StructGenerator {
             syntax.push_str("struct ");
 
             if let Some(struct_name) = &item.name {
-                let (generic_params, where_predicates) = GenericGenerator::generate_generics(&generics)?;
+                let (generic_params, where_predicates) =
+                    GenericGenerator::generate_generics(&generics)?;
+                syntax.push_str(struct_name);
+                syntax.push_str(&generic_params);
                 match kind {
                     StructKind::Unit => {
-                        syntax.push_str(struct_name);
-                        syntax.push_str(&generic_params);
-                        syntax.push_str(&where_predicates);
                         syntax.push_str(";");
                     }
                     StructKind::Tuple(unnamed_fields) => {
-                        // TODO
+                        syntax.push_str("(");
+                        for (i, unnamed_field) in unnamed_fields.iter().enumerate() {
+                            if i != 0 {
+                                syntax.push_str(", ");
+                            }
+
+                            if let Some(field_id) = unnamed_field {
+                                let Some(field_item) = index.get(field_id) else {
+                                    return Err(anyhow::Error::msg(format!(
+                                        "Failed to find item with id: {} in index",
+                                        field_id.0
+                                    )));
+                                };
+                                let ItemEnum::StructField(type_) = &field_item.inner else {
+                                    return Err(anyhow::Error::msg(format!("inner can't be anything other than `StructField in index id: {}", field_id.0)));
+                                };
+                                syntax.push_str(&TypeGenerator::type_to_string(type_));
+                            } else {
+                                syntax.push_str("/* private field */");
+                            }
+                        }
+                        syntax.push_str(")");
                     }
                     StructKind::Plain {
                         fields,
                         has_stripped_fields,
                     } => {
-                        // TODO
+                        syntax.push_str(" {\n");
+                        for field_id in fields {
+                            let Some(field_item) = index.get(field_id) else {
+                                return Err(anyhow::Error::msg(format!(
+                                    "Failed to find item with id: {} in index",
+                                    field_id.0
+                                )));
+                            };
+                            let Some(field_name) = &field_item.name else {
+                                return Err(anyhow::Error::msg(format!(
+                                    "Failed to find name of struct field with id: {} in index",
+                                    field_id.0
+                                )));
+                            };
+                            let ItemEnum::StructField(type_) = &field_item.inner else {
+                                return Err(anyhow::Error::msg(format!("inner can't be anything other than `StructField in index id: {}", field_id.0)));
+                            };
+
+                            syntax.push_str("\t");
+                            syntax.push_str(&VisibilityGenerator::generate_visibility(
+                                &field_item.visibility,
+                            ));
+                            syntax.push_str(field_name);
+                            syntax.push_str(": ");
+                            syntax.push_str(&TypeGenerator::type_to_string(type_));
+                            syntax.push_str(",\n");
+                        }
+
+                        if *has_stripped_fields {
+                            syntax.push_str("\t/* private fields */\n");
+                        }
+                        syntax.push_str("}");
                     }
                 }
+                syntax.push_str(&where_predicates);
                 syntax.push_str("\n```\n");
-                return Ok(syntax)
+                return Ok(syntax);
             }
-            return Err(anyhow::Error::msg("Can't document a struct with no name"))
+            return Err(anyhow::Error::msg("Can't document a struct with no name"));
         }
         Err(anyhow::Error::msg(
             "Umm... Only Item with inner ItemEnum::Struct to be used here",
